a92e14d6fd610459f194bed1af22e4e03daca27b
Gabriel Damião <gabrieldamiao338@gmail.com>
2026-08-02 18:28:50 -0300

    JS

--- script.js
+++ script.js
@@ -243,7 +243,7 @@ setInterval(() => {
     if (animando) return;
     indice += passo;
     atualizar();
-}, 4000);
+}, 6000);
 window.addEventListener('resize', () => {
     passo = qtdCards();
     atualizar(false);
